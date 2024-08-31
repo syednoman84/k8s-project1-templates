f13aea79acb0d9111d4894dcf2d631b7403fdef4
syedukasha@hotmail.com <syednoman84@hotmail.com>
2024-08-31 10:06:17 -0400

    updates

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # vagrantfile
 
 This files contains instructions to spin up master node and worker nodes. The number of master nodes and worker nodes is defined by NUM_MASTER_NODE and NUM_WORKER_NODE variables. Note that it also runs `setup-hosts.sh` and `update-dns.sh` scripts on all the VMs after setting up the VMs.
+
+# network-docker-k8s-installation.sh
+
+This takes care of several network settings and installs docker and kubernetes on the VM.
---
+++ network-docker-k8s-installation.sh
@@ -0,0 +1,107 @@
+# This script makes some network related changes to the VM and installs docker and kubernetes.
+# If you want to not install kubernetes or docker, you can comment out the tasks from 6 to 9.
+export DEBIAN_FRONTEND=noninteractive
+
+echo "----------------------------------"
+echo "[TASK 1] show whoami"
+echo "----------------------------------"
+whoami
+systemctl disable --now ufw >/dev/null 2>&1
+
+echo "----------------------------------"
+echo "[TASK 2] Stop and Disable firewall"
+echo "----------------------------------"
+systemctl disable --now ufw >/dev/null 2>&1
+
+echo "----------------------------------"
+echo "[TASK 3] Letting iptables see bridged traffic"
+echo "----------------------------------"
+modprobe br_netfilter
+
+echo "----------------------------------"
+echo "[TASK 4] Enable and Load Kernel modules"
+echo "----------------------------------"
+cat >>/etc/modules-load.d/k8s.conf<<EOF
+br_netfilter
+EOF
+
+echo "----------------------------------"
+echo "[TASK 5] Add Kernel settings"
+echo "----------------------------------"
+cat >>/etc/sysctl.d/k8s.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+echo "----------------------------------"
+echo "[TASK 6] Installing docker"
+echo "----------------------------------"
+apt-get update
+apt-get install -y \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg \
+lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+     "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
+
+echo "----------------------------------"
+echo "[TASK 7] Configure the Docker daemon, in particular to use systemd for the management of the container’s cgroups."
+echo "----------------------------------"
+mkdir /etc/Docker
+cat >>/etc/docker/daemon.json<<EOF
+{
+"exec-opts": ["native.cgroupdriver=systemd"],
+"log-driver": "json-file",
+"log-opts": {
+    "max-size": "100m"
+},
+"storage-driver": "overlay2"
+}
+EOF
+systemctl enable docker
+systemctl daemon-reload
+systemctl restart docker
+
+# Optionally you can choose to install crio instead of docker if you want as container runtime for k8s
+# sudo apt-get update -y
+# sudo apt-get install -y cri-o
+
+# sudo systemctl daemon-reload
+#sudo systemctl enable crio --now
+#sudo systemctl start crio.service
+
+# echo "CRI runtime installed successfully"
+
+echo "----------------------------------"
+echo "[TASK 8] Installing dependencies for k8s"
+echo "----------------------------------"
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gpg -y
+
+echo "----------------------------------"
+echo "[TASK 9] Installing k8s"
+echo "----------------------------------"
+# In releases older than Debian 12 and Ubuntu 22.04, directory /etc/apt/keyrings does not exist by default, 
+# and it should be created before the curl command.
+sudo mkdir -p -m 755 /etc/apt/keyrings
+
+# Add the appropriate Kubernetes apt repository. 
+# Please note that this repository have packages only for Kubernetes 1.31; 
+# for other Kubernetes minor versions, you need to change the Kubernetes minor version in the URL to match your desired minor version
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Update the apt package index, install kubelet, kubeadm and kubectl, and pin their version:
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
